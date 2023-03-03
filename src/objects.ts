957b7000a71f08e7faf65abf4f191e163a9c45d6
MarinaSenina <frau.senina32@yandex.ru>
2023-03-03 16:16:57 +0300

    finished course

--- src/objects.ts
+++ src/objects.ts
@@ -19,6 +19,10 @@ admin = {
     age: 54,
     nickName: "crazyAdmin"
 }
+
+
+
+
 // подходит для описания любого типа данных
 type Name = string;
 
@@ -44,6 +48,15 @@ let ivan: IWorker = {
 
 console.log(ivan.age)
 
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+
+
+// наследование от нескольких интерфейсов
 interface IUser {
     name: string,
     age: number,
@@ -62,3 +75,16 @@ class Anton implements IPass, IUser {
     }
 
 }
+
+
+
+// Откидывает лишние параметры при утиной типизации
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+const point3 = { x: 12, y: 26, z: 89 };
+logPoint(point3); // logs "12, 26"
+
+const rect = { x: 33, y: 3, width: 30, height: 80 };
+logPoint(rect);
